1a899f83167e5234334a7eb71ddedaa577892f9a
Captain-5415 <gowtham996644@gmail.com>
2026-06-29 13:37:55 +0530

    Minor UI improvements

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,36 +1,44 @@
 import { Link, NavLink } from "react-router-dom";
-import { useContext } from "react";
+import { useContext, useState } from "react";
 import { ThemeContext } from "../context/ThemeContext";
+import { FaBars, FaMoon, FaSun, FaTimes } from "react-icons/fa";
 import "./Navbar.css";
 import logo from "../assets/hospital-logo.png";
 
 function Navbar() {
   const { theme, toggleTheme } = useContext(ThemeContext);
+  const [menuOpen, setMenuOpen] = useState(false);
 
   return (
     <nav className={`navbar ${theme}`}>
       <div className="logo">
         <Link to="/">
           <img src={logo} alt="GCube Hospital Logo" className="logo-img" />
-          <span>GCube<br/>Hospital</span>
+          <span>GCube<br />Hospital</span>
         </Link>
       </div>
 
-      <ul className="nav-links">
-        <li><NavLink to="/">Home</NavLink></li>
-        <li><NavLink to="/about">About</NavLink></li>
-        <li><NavLink to="/doctors">Doctors</NavLink></li>
-        <li><NavLink to="/symptom">Symptom Checker</NavLink></li>
-        <li><NavLink to="/appointments">Appointments</NavLink></li>
-        <li><NavLink to="/services">Services</NavLink></li>
-        <li><NavLink to="/contact">Contact</NavLink></li>
+      <ul className={`nav-links ${menuOpen ? "open" : ""}`}>
+        <li><NavLink to="/" onClick={() => setMenuOpen(false)}>Home</NavLink></li>
+        <li><NavLink to="/about" onClick={() => setMenuOpen(false)}>About</NavLink></li>
+        <li><NavLink to="/doctors" onClick={() => setMenuOpen(false)}>Doctors</NavLink></li>
+        <li><NavLink to="/symptom" onClick={() => setMenuOpen(false)}>Symptom Checker</NavLink></li>
+        <li><NavLink to="/appointments" onClick={() => setMenuOpen(false)}>Appointments</NavLink></li>
+        <li><NavLink to="/services" onClick={() => setMenuOpen(false)}>Services</NavLink></li>
+        <li><NavLink to="/contact" onClick={() => setMenuOpen(false)}>Contact</NavLink></li>
       </ul>
 
-      <button
-        className="theme-btn"
-        onClick={toggleTheme}>
-        {theme === "light" ? " Dark Theme" : " Light Theme"}
+      <button className="theme-btn" onClick={toggleTheme}>
+        {theme === "light" ? <FaMoon /> : <FaSun />}
       </button>
+
+      <button
+        className="menu-btn"
+        onClick={() => setMenuOpen(!menuOpen)}
+      >
+        {menuOpen ? <FaTimes /> : <FaBars />}
+      </button>
+      
     </nav>
   );
 }
--- src/context/ThemeContext.jsx
+++ src/context/ThemeContext.jsx
@@ -5,7 +5,7 @@ export const ThemeContext = createContext();
 export default function ThemeProvider({ children }) {
 
     const [theme, setTheme] = useState(
-        localStorage.getItem("theme") || "light"
+        localStorage.getItem("theme") || "dark"
     );
 
     useEffect(() => {
